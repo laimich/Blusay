997e6439cccb513d31e29bed9b0abc09c3aef7e7
Michelle <laimich3@gmail.com>
2017-07-17 01:28:26 -0700

    Started a response, fixed build problems

--- app/src/main/java/moodlistener/blusay/MoodModel.java
+++ app/src/main/java/moodlistener/blusay/MoodModel.java
@@ -10,7 +10,7 @@ public class MoodModel {
     //for current user
     private Account currentUser;
     private Mood selectedMood;
-
+    private String display; //can be "light" or "dark"
 
     //for saved info
     private AccountUnlocked unlockedAccount;
---
+++ app/src/main/java/moodlistener/blusay/ResponseMusic.java
@@ -0,0 +1,67 @@
+package moodlistener.blusay;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import static java.sql.DriverManager.println;
+
+
+public class ResponseMusic extends AppCompatActivity {
+
+    @Override @TargetApi(23)
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.response_music_alt);
+
+        final MediaPlayer mp = new MediaPlayer();   //make static if have multi mp3 files to play
+        ImageButton playPause = (ImageButton) findViewById(R.id.responseMusicPlayPause);     //this would be the button to play/stop
+        TextView timestamp = (TextView) findViewById(R.id.responseMusicTimestamp);
+        timestamp.setText(mp.getTimestamp().toString());
+
+        playPause.setOnClickListener(new View.OnClickListener() {
+
+            @Override @TargetApi(21)
+            public void onClick(View v) {
+
+                ImageButton playReplay = (ImageButton) findViewById(R.id.responseMusicPlayPause);
+
+                if(mp.isPlaying())
+                {
+                    playReplay.setBackground(getDrawable(R.drawable.play_circle));
+                    mp.stop();
+                }
+
+                try {
+                    playReplay.setBackground(getDrawable(R.drawable.replay_arrow));
+                    mp.reset();
+                    AssetFileDescriptor afd;
+                    afd = getAssets().openFd("AudioFile.mp3");   //this is name of mp3 file in assets folder
+                    mp.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+                    mp.prepare();
+                    mp.start();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+
+
+}
